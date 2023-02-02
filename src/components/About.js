649302c3e8c80a49d77df8f1f9fe2f82fba1ca93
ajin3830 <ajin3830@gmail.com>
2023-02-02 11:20:43 -0500

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,26 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* this test is weird */}
+      {/* {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null} */}
+      {props.bio.length >= 1 ? <p>{props.bio}</p> : null}
+      {/* <p>{props.bio = "" ? null : props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
 
+
 export default About;
+
+// if props empty string
+  // dont pass down
+  // else
+  // pass down props
